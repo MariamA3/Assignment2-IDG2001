375b8d674dbca2eadd56b6d83e0c772059ec6bdd
SebastianHaugen <sebassha@stud.ntnu.no>
2024-05-15 22:01:06 +0200

    More frontend design

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,14 @@
+import GetPosts from "../components/GetPosts";
+import "../styles/Home.css";
+
 function Home() {
     return (
-        <div>
+        <div className="home-container">
             <h1>Home</h1>
-            <p>Recent posts in dogs:</p>
+            <p>Recent posts:</p>
             {/* Here vi can insert the posts for a category */}
             {/* I dont know which one we should show when the user first load*/}
+            <GetPosts />
         </div>
     );
 }
