0b6af693818178f821cd8239761c4a6161599697
SebastianHaugen <sebassha@stud.ntnu.no>
2024-05-16 23:42:54 +0200

    Setting up axios and fixes to getCategories

--- frontend/src/components/GetCategories.js
+++ frontend/src/components/GetCategories.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from "react";
+import Loading from "../components/Loading";
+import axiosInstance from "../api/axios";
 import "../styles/GetCategories.css";
 
 function GetCategories() {
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  // Fetch the data on mount and set the categories
   useEffect(() => {
-    fetch("http://localhost:4000/categories")
-      .then((response) => response.json())
-      .then((data) => setCategories(data));
+    axiosInstance
+      .get("/categories")
+      .then((response) => {
+        setCategories(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
   }, []);
+
+  // If data is still loading, display a loading screen
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className="category-container">
-       <ul>
+      <ul>
         {categories.map((category, index) => (
           <li key={index}>
-            b/{category.name}
+            <p>b/{category.name}</p>
           </li>
         ))}
       </ul>
