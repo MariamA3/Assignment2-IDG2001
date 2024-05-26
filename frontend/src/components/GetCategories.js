0c729492202f71919de91fe85da37fd234653891
MariamA3 <mariamab@stud.ntnu.no>
2024-05-26 22:28:19 +0200

    changed error shown in the console, see if it fixes in the vm

--- frontend/src/components/GetCategories.js
+++ frontend/src/components/GetCategories.js
@@ -5,14 +5,11 @@ import Loading from "../components/Loading";
 import axiosInstance from "../api/axios";
 import "../styles/GetCategories.css";
 
-// GetCategories component to fetch categories and display them to the user
-function GetCategories({closeMenu}) {
+function GetCategories({ closeMenu }) {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // Fetch the categories when the component mounts
   useEffect(() => {
-    // Set loading to true before starting to fetch data
     axiosInstance
       .get("/categories")
       .then((response) => {
@@ -20,34 +17,29 @@ function GetCategories({closeMenu}) {
         setLoading(false);
       })
       .catch((error) => {
-        // Log the error and stop loading even if an error occurs
         setLoading(false);
         toast.error("Failed to fetch categories. Please try again later.");
         console.error("Error fetching data: ", error);        
       });
   }, []);
 
-  // Display a loading spinner while fetching data
   if (loading) {
     return <Loading />;
   }
 
-// Display the categories if they are fetched successfully
-return (
-  <div className="category-container">
-    <ul>
-      {/* Check if categories is an array before mapping */}
-      {Array.isArray(categories) && categories.map((category, index) => (
-        <li key={index}>
-          {/* Use category.category_id instead of category_id */}
-          <Link to={`/b/${category.name}`} onClick={closeMenu}>
-            <p>b/{category.name}</p>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
+  return (
+    <div className="category-container">
+      <ul>
+        {categories.map((category, index) => (
+          <li key={index}>
+            <Link to={`/b/${category.name}`} onClick={closeMenu}>
+              <p>b/{category.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default GetCategories;
